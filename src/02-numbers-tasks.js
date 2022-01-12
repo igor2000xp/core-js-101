a75e8270bb20356915e05a7d34c5730e9193d8db
igor2000xp <igorring@gmail.com>
2022-01-13 01:01:24 +0300

    feat: add all tasks of string and 11 tasks of numbers topic

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -95,10 +95,12 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+// function getLinearEquationRoot(/* a, b */) {
+//   throw new Error('Not implemented');
+// }
+function getLinearEquationRoot(a, b) {
+  return (-b) / a;
 }
-
 
 /**
  * Returns an angle (in radians) between two vectors given by xi and yi,
@@ -118,8 +120,13 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+// function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
+//   throw new Error('Not implemented');
+// }
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const up = (x1 * x2) + (y1 * y2);
+  const down = (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2));
+  return Math.acos(up / down);
 }
 
 /**
@@ -134,8 +141,13 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+// function getLastDigit(/* value */) {
+//   throw new Error('Not implemented');
+// }
+function getLastDigit(value) {
+  const returnValue = String(value).split('');
+  const indexValue = returnValue.length;
+  return returnValue[indexValue - 1];
 }
 
 
@@ -150,8 +162,11 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+// function parseNumberFromString(/* value */) {
+//   throw new Error('Not implemented');
+// }
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -167,8 +182,11 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+// function getParallelepipedDiagonal(/* a, b, c */) {
+//   throw new Error('Not implemented');
+// }
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 
@@ -189,8 +207,21 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+// function roundToPowerOfTen(/* num, pow */) {
+//   return ;
+// }
+function roundToPowerOfTen(num, pow) {
+  let returnNum;
+  if ((num % ((10) ** pow)) / ((10) ** pow) > 0.5) {
+    returnNum = num - (num % ((10) ** pow)) + (10) ** pow;
+  } else {
+    returnNum = num - (num % ((10) ** pow));
+  }
+
+  if (pow === 0) {
+    returnNum = num;
+  }
+  return returnNum;
 }
 
 /**
@@ -210,8 +241,12 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let isPrimeSimple = true;
+  for (let i = 1; i < n - 1; i += 1) {
+    if (((n - 0.0) % (n - i)) === 0) isPrimeSimple = false;
+  }
+  return isPrimeSimple;
 }
 
 /**
