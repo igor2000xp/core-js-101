dcef1914f900ea3ddfd2d06331238d6b4e95ba25
igor2000xp <igorring@gmail.com>
2022-01-13 03:10:21 +0300

    fix: add all tasks of string and numbers topics

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -268,7 +268,6 @@ function toNumber(value, def) {
   const num = Number(value);
   let returnValue = def;
   if (typeof num === 'number' && !Number.isNaN(num)) returnValue = num;
-  console.log(returnValue);
   return returnValue;
 }
 
