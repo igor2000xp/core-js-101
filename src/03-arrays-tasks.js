868cbcfd189eba0f02f16fcbf3abd5de56fc26bc
igor2000xp <igorring@gmail.com>
2022-01-15 01:09:43 +0300

    feat: add 9 tasks of arrays topics

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -396,8 +396,12 @@ function getFalsyValuesCount(/* arr */) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+// function findAllOccurrences(/* arr, item */) {
+//   throw new Error('Not implemented');
+// }
+function findAllOccurrences(arr, item) {
+  const result = arr.filter((it) => it === item);
+  return result.length;
 }
 
 /**
