e08e647dbad660834c9ac3ef95ae6c50ef8c6b13
igor2000xp <igorring@gmail.com>
2022-01-15 01:02:31 +0300

    feat: add 8 tasks of arrays topics

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -97,8 +97,11 @@ function getArrayOfPositives(arr) {
  *    [ 1, 2, 3, 4, 5 ] => []
  *    [ 'cat, 'dog', 'raccoon' ] => [ 'cat', 'dog', 'raccoon' ]
  */
-function getArrayOfStrings(/* arr */) {
-  throw new Error('Not implemented');
+// function getArrayOfStrings(/* arr */) {
+//   throw new Error('Not implemented');
+// }
+function getArrayOfStrings(arr) {
+  return arr.filter((item) => typeof item === 'string');
 }
 
 /**
@@ -114,8 +117,11 @@ function getArrayOfStrings(/* arr */) {
  *    [ 1, 2, 3, 4, 5, 'false' ]         => [ 1, 2, 3, 4, 5, 'false' ]
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+// function removeFalsyValues(/* arr */) {
+//   throw new Error('Not implemented');
+// }
+function removeFalsyValues(arr) {
+  return arr.filter((item) => item);
 }
 
 /**
@@ -129,8 +135,11 @@ function removeFalsyValues(/* arr */) {
  *    => [ 'PERMANENT-INTERNSHIP', 'GLUTINOUS-SHRIEK', 'MULTIPLICATIVE-ELEVATION' ],
  *    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]  => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
  */
-function getUpperCaseStrings(/* arr */) {
-  throw new Error('Not implemented');
+// function getUpperCaseStrings(/* arr */) {
+//   throw new Error('Not implemented');
+// }
+function getUpperCaseStrings(arr) {
+  return arr.map((item) => item.toUpperCase());
 }
 
 
@@ -144,8 +153,11 @@ function getUpperCaseStrings(/* arr */) {
  *    [ '', 'a', 'bc', 'def', 'ghij' ]  => [ 0, 1, 2, 3, 4 ]
  *    [ 'angular', 'react', 'ember' ] => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+// function getStringsLength(/* arr */) {
+//   throw new Error('Not implemented');
+// }
+function getStringsLength(arr) {
+  return arr.map((item) => item.length);
 }
 
 /**
